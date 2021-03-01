416b76d6ab8c79648d0668db5d68d6d257bcf05c
adamtuenti <adanavarrete15@gmail.com>
2021-03-01 11:51:47 -0500

    cambios

---
+++ parteWeb/src/app/shared/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  usuario;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
