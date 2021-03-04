e52a1b71c99c182d05a2c4ed455599e15adba746
Renzo <rbloor@espol.edu.ec>
2021-03-04 16:06:18 -0500

    changes

--- parteWeb/src/app/home/home.component.ts
+++ parteWeb/src/app/home/home.component.ts
@@ -8,8 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
-
+  msg:string;
   ngOnInit(): void {
+  }
+
+  eventR1(){
+    this.msg='Costa works!';
+    return this.msg;
+  }
+  eventR2(){
+    this.msg='Sierra works!';
+    return this.msg;
+  }
+  eventR3(){
+    this.msg='Amazonia works!';
+    return this.msg;
   }
 
 }
