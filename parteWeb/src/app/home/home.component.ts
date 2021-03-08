3fd436e682532594880ff5ed1900a9c5ee978170
Angel <AngelJumbo>
2021-03-07 21:07:19 -0500

    paso de region entre la pagina de inicio y la de productos

--- parteWeb/src/app/home/home.component.ts
+++ parteWeb/src/app/home/home.component.ts
@@ -17,53 +17,41 @@ export class HomeComponent implements OnInit {
   }
 
   goCosta(): void{
-    this.router.navigate(['/', 'productos']);
+    this.router.navigate(['/', 'productos',{region:"costa"}]);
   }
 
   goSierra(): void{
-    this.router.navigate(['/', 'productos']);
+    this.router.navigate(['/', 'productos',{region:"sierra"}]);
   }
 
   goAmazonia(): void{
-    this.router.navigate(['/', 'productos']);
+    this.router.navigate(['/', 'productos',{region:"amazonia"}]);
   }
 
 
 
 
   costaOver(): void{
-
-    console.log("hola");
     this.renderer.setAttribute(this.childComp.nativeElement,"stroke","#F7F16D");
     this.renderer.setAttribute(this.childComp.nativeElement,"fill","#74CB1C");
-
   }
   costaOut(): void{
-    console.log("chao");
     this.renderer.setAttribute(this.childComp.nativeElement,"stroke","#fff");
     this.renderer.setAttribute(this.childComp.nativeElement,"fill","#F7F16D");
   }
   sierraOver(): void{
-
-    console.log("hola");
     this.renderer.setAttribute(this.childComp2.nativeElement,"stroke","#7fff7f");
     this.renderer.setAttribute(this.childComp2.nativeElement,"fill","#74CB1C");
-
   }
   sierraOut(): void{
-    console.log("chao");
     this.renderer.setAttribute(this.childComp2.nativeElement,"stroke","#fff");
     this.renderer.setAttribute(this.childComp2.nativeElement,"fill","#A0FE6D");
   }
   amazoniaOver(): void{
-
-    console.log("hola");
     this.renderer.setAttribute(this.childComp3.nativeElement,"stroke","#7fff7f");
     this.renderer.setAttribute(this.childComp3.nativeElement,"fill","#74CB1C");
-
   }
   amazoniaOut(): void{
-    console.log("chao");
     this.renderer.setAttribute(this.childComp3.nativeElement,"stroke","#fff");
     this.renderer.setAttribute(this.childComp3.nativeElement,"fill","#9A7C31");
   }
--- parteWeb/src/app/productos/productos.component.ts
+++ parteWeb/src/app/productos/productos.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from "@angular/fire/firestore";
 import {CategoriasProductosService} from "../../services/categorias-productos.service";
 import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute } from '@angular/router'; 
 
 @Component({
   selector: 'app-productos',
@@ -11,11 +12,20 @@ import { MatTableDataSource } from '@angular/material/table';
 export class ProductosComponent implements OnInit {
 
   public categorias = [];
+  public regionSeleccionada: String; 
 
-  constructor(private categoriasServices: CategoriasProductosService) { }
+  constructor(private activatedroute:ActivatedRoute,private categoriasServices: CategoriasProductosService) { 
+    
+
+  }
 
   ngOnInit(): void {
-    this.getCategorias();
+    /*this.activatedroute.paramMap.subscribe( param=>{
+      this.regionSeleccionada = param.get("region");
+      console.log(this.regionSeleccionada);
+    })*/
+    this.regionSeleccionada = this.activatedroute.snapshot.paramMap.get("region");
+    console.log(this.regionSeleccionada);
   }
 
   getCategorias(): void {
